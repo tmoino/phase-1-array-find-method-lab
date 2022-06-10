f939905ef8658624b4552b4b59207bea151a20bc
tmoino <tony.moino@student.moringaschool.com>
2022-06-10 20:34:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin (record){
+    const result = record.find( record => record.result === "W" );
+    //const rslt = record.find( record => record.rslt === "W" );
+    if (!!result){
+        return result.year;
+    }
+    else{
+        return undefined;
+    }
+
+}
